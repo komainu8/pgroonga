259340bb7bd4d0ba5d79a2fe9ad43f0ea469c3b1
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-02-02 21:05:33 +0900

    pgroonga: remove pgroonga.float4_ops operator class
    GitHub: GH-647
    
    This is part of the task of remove "pgroonga" schema. It's deprecated since PGroonga 2.
    This is incompatible with PGroonga 3.x or earlier.
    
    TODO:
    * Test
    * Resolve confrict

--- data/pgroonga.sql
+++ data/pgroonga.sql
@@ -3179,14 +3179,6 @@ BEGIN
 				OPERATOR 4 >=,
 				OPERATOR 5 >;
 
-		CREATE OPERATOR CLASS pgroonga.float4_ops FOR TYPE float4
-			USING pgroonga AS
-				OPERATOR 1 <,
-				OPERATOR 2 <=,
-				OPERATOR 3 =,
-				OPERATOR 4 >=,
-				OPERATOR 5 >;
-
 		CREATE OPERATOR CLASS pgroonga.float8_ops FOR TYPE float8
 			USING pgroonga AS
 				OPERATOR 1 <,
