6245c151de305bf713a158023279ee2fd0c1b7ad
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-02-19 12:14:41 +0900

    Test

--- src/pgroonga.c
+++ src/pgroonga.c
@@ -54,6 +54,7 @@
 #include <nodes/nodeFuncs.h>
 #include <optimizer/optimizer.h>
 #include <pgstat.h>
+#include <stdbool.h>
 #include <storage/bufmgr.h>
 #include <storage/ipc.h>
 #include <storage/latch.h>
@@ -5391,6 +5392,14 @@ PGrnSearchIsInCondition(ScanKey key)
 			key->sk_strategy == PGrnEqualStrategyNumber);
 }
 
+static bool
+PGrnSearchIsMatchIn(ScanKey key)
+{
+	return (key->sk_flags & SK_SEARCHARRAY) &&
+		   ((key->sk_strategy == PGrnMatchStrategyNumber)
+			|| (key->sk_strategy == PGrnMatchStrategyV2Number));
+}
+
 static void
 PGrnSearchBuildConditionIn(PGrnSearchData *data,
 						   ScanKey key,
@@ -6122,6 +6131,13 @@ PGrnSearchBuildCondition(Relation index, ScanKey key, PGrnSearchData *data)
 	{
 		PGrnSearchBuildConditionIn(data, key, targetColumn, attribute);
 		return;
+	}
+	if (PGrnSearchIsMatchIn(key))
+	{
+		if (key->sk_strategy != PGrnMatchInStrategyV2Number)
+		{
+			key->sk_strategy = PGrnMatchInStrategyV2Number;
+		}
 	}
 
 	if (key->sk_strategy == PGrnMatchFTSConditionStrategyV2Number ||
